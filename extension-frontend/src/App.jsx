99d731993fc3137ba8cfbb700c695e21721faf52
Ritesh <riteshpandit1708@gmail.com>
2025-07-22 18:20:40 +0530

    replaced the content of App jsx with env var

--- extension-frontend/src/App.jsx
+++ extension-frontend/src/App.jsx
@@ -32,6 +32,7 @@ import {
 import "./App.css";
 
 // --- Firebase Configuration ---
+// --- Firebase Configuration ---
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
